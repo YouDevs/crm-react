46eeb5ec2969039df7db94133786d5325c3c16bb
Carlos Hernández <carlos_develops@outlook.com>
2023-07-24 07:15:15 -0600

    creando un loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,17 @@
-
+// Proviene: de react-router-dom que estamos usando en el archivo: main.jsx
+// Es muy similar a useEffect
+// Se ejecutará cuando el componente cargue: ideal para cargar state o consultar una api y obtener resultado que quieras mostrar en un componente.
+export function loader() {
+  return "Desde loader";
+}
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-        <h1>Clientes</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+    </>
+
   )
 }
 
