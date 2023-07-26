2794ee6a12889f392566938d0da5f1dce3644465
Carlos Hernández <carlos_develops@outlook.com>
2023-07-26 06:55:14 -0600

    agregando datos al loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,57 @@
+import {useLoaderData} from 'react-router-dom'
+
 // Proviene: de react-router-dom que estamos usando en el archivo: main.jsx
 // Es muy similar a useEffect
 // Se ejecutará cuando el componente cargue: ideal para cargar state o consultar una api y obtener resultado que quieras mostrar en un componente.
 export function loader() {
-  return "Desde loader";
+  // Esto ya se convierte en mi satate:
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+
+  return clientes;
 }
 
 const Index = () => {
+
+  const clientes = useLoaderData();
+
+  console.log(clientes)
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
