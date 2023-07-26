f5f3b80f40246ce19ac7f306108499607f9b7644
Carlos Hernández <carlos_develops@outlook.com>
2023-07-26 07:08:22 -0600

    iterando clientes y mostrando la informacion

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import {useLoaderData} from 'react-router-dom'
+import Cliente from '../components/Cliente'
 
 // Proviene: de react-router-dom que estamos usando en el archivo: main.jsx
 // Es muy similar a useEffect
@@ -47,7 +48,7 @@ export function loader() {
 }
 
 const Index = () => {
-
+  // Recupera los datos del loader
   const clientes = useLoaderData();
 
   console.log(clientes)
@@ -56,6 +57,27 @@ const Index = () => {
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tus clientes</p>
+
+      { clientes.length ? (
+        <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-blue-800 text-white'>
+            <tr>
+              <th className='p-2'>Cliente</th>
+              <th className='p-2'>Contacto</th>
+              <th className='p-2'>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+              {clientes.map( cliente => (
+                <Cliente cliente={cliente} key={cliente.id}/>
+
+              )) }
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">No hay clientes aun.</p>
+      ) }
+
     </>
 
   )
